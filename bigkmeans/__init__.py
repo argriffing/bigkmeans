456ffd5a27bd3c3c29728f7067f81a94b2819bdb
alex <argriffi@ncsu.edu>
2013-01-22 13:37:19 -0500

    add more boilerplate files and codes so that nosetests will find our tests

--- bigkmeans/__init__.py
+++ bigkmeans/__init__.py
@@ -1,9 +1,12 @@
+from bigkmeans import *
+
+import lloyd
+
+# import this package so that nosetests can find our tests
+import test
+
 
 # this testing idea is from algopy
 from numpy.testing import Tester
 test = Tester().test
-
-from bigkmeans import *
-
-import lloyd
 
